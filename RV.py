04764ecff90ca2068e3badc8e27191316a46af86
Chetan Tewari <60449828+chetanupes@users.noreply.github.com>
2023-04-23 11:40:37 -0600

    final

--- RV.py
+++ RV.py
@@ -356,7 +356,7 @@ if Select_Method=='Acceptance-Rejection':
         
             fig = px.histogram(X)
             fig.update_yaxes(ticklabelposition="inside top", title='Frequency')
-        fig.update_xaxes(ticklabelposition="inside top", title='{} Random Variate'.format(Select_Dist))
+            fig.update_xaxes(ticklabelposition="inside top", title='{} Random Variate'.format(Select_Dist))
             fig.update_layout(showlegend=False,height=500, width=1300,barmode='group',title_text='Histogram: {} RV'.format(Select_Dist))
 
             #Plot!
